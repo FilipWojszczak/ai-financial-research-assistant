681a31106e515d64ea9f7b5c679e7aadab027e10
FilipWojszczak <62936153+FilipWojszczak@users.noreply.github.com>
2026-03-27 17:26:29 +0000

    feat(api): create User model and schemas for endpoints

---
+++ api/schemas/auth.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, ConfigDict, EmailStr, Field
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=8)
+    model_config = ConfigDict(extra="forbid")
+
+
+class UserRead(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"  # noqa: S105
